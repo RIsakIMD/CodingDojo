9b16fc479fa755f2049fe5bd81ebd60fee83de37
SgtJoe <roman_isakov@outlook.com>
2023-07-09 21:29:58 -0700

    First JSX

---
+++ MERN/React/01_IntroToReact/02_FirstJSX/first-jsx/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <h2>My to-do list:</h2>
+      <ol>
+        <li>Learn React</li>
+        <li>Become a React Master</li>
+        <li>Success!</li>
+      </ol>
+    </>
+  )
+}
+
+export default App
